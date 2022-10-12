231cb33844b6c9c7a0fd333b3b885788d6c38be7
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-11 22:08:40 -0700

    app complete

--- src/App.js
+++ src/App.js
@@ -40,37 +40,37 @@ const searchLocation = (event) =>{
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()} F</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
-
-        {data.name !== undefined && 
-
+        {data.name !== undefined && (
           <div className="bottom">
-          <div className="feels">
-            {data.main ? <p className="bold">{data.main.feels_like} F</p> : null}
-            <p>Feels Like</p>
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()} F</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humdity">
+              {data.main ? (
+                <p className="bold">{data.main.humidity.toFixed()} %</p>
+              ) : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.main ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humdity">
-            {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            {data.main ? <p className="bold">{data.wind.speed} MPH</p> : null}
-            <p>Wind Speed</p>
-          </div>
-        </div>
-      
-    }
-       
+        )}
+      </div>
     </div>
-    </div>
-
-
   );
 }
 
