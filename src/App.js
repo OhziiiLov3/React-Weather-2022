d9510fc375aaa62e99ac6b8a4b0797b5f4d1a2e5
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-11 20:49:43 -0700

    axios installed weather api working in console

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react'
-// import {useState} from 'react'
+import axios from 'axios';
+import React, {useState}  from 'react'
+
 
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={233d7e0ca24b8004061ee0c5dab4fa94}`; 
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=233d7e0ca24b8004061ee0c5dab4fa94`; 
+
+const searchLocation = (event) =>{
+  if(event.key === 'Enter'){
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+
+  }
+
+}
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
